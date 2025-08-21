196ab1c1e4794e20539cf1ee3939cf6237921487
Daniel Herrera <dher426@wgu.edu>
2025-08-21 01:02:06 -0700

    a working version of the app, prior to any changes while implementing azure storage functionality.

--- src/GameStore.Api/obj/Release/net8.0/GameStore.Api.AssemblyInfo.cs
+++ src/GameStore.Api/obj/Release/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e65a810b502a2adc18084fc5549343b73de0bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
